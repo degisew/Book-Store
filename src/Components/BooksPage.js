c5890b6a9d126b2b5d87c18ca95ecf2a1a723938
Degisew <jegisew21@gmail.com>
2022-11-11 20:07:56 +0300

    cutted out dummy data and move to redux store

--- src/Components/BooksPage.js
+++ src/Components/BooksPage.js
@@ -1,41 +1,12 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import AddBook from './AddBook';
-import Book from './Book';
 
 function BooksPage() {
-  const books = [
-    {
-      id: uuidv4(),
-      title: 'Zamra',
-      author: 'Yismake',
-    },
-    {
-      id: uuidv4(),
-      title: 'Merbebit',
-      author: 'Alemayehu',
-    },
-    {
-      id: uuidv4(),
-      title: 'Dertogada',
-      author: 'Yismake',
-    },
-    {
-      id: uuidv4(),
-      title: 'Zubeyda',
-      author: 'Alex',
-    },
-    {
-      id: uuidv4(),
-      title: 'Hashmal',
-      author: 'Maebel',
-    },
-  ];
   return (
     <div className="books-container">
-      {books.map((book) => (
+      {/* {books.map((book) => (
         <Book title={book.title} key={book.id} />
-      ))}
+      ))} */}
       <AddBook />
     </div>
   );
