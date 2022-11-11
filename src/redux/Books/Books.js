075f53e86f6d4fa0e4b5d6d301c0f151ea1291c3
Degisew <jegisew21@gmail.com>
2022-11-11 20:08:29 +0300

    add a list of books to store

--- src/redux/Books/Books.js
+++ src/redux/Books/Books.js
@@ -1,10 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
 const bookSlice = createSlice({
   name: 'add_remove_book',
   initialState: {
-    books: [],
+    books: [
+      {
+        id: uuidv4(),
+        title: 'Zamra',
+        author: 'Yismake',
+      },
+      {
+        id: uuidv4(),
+        title: 'Merbebit',
+        author: 'Alemayehu',
+      },
+      {
+        id: uuidv4(),
+        title: 'Dertogada',
+        author: 'Yismake',
+      },
+      {
+        id: uuidv4(),
+        title: 'Zubeyda',
+        author: 'Alex',
+      },
+    ],
   },
+
   reducers: {
     // reducers and also action creators at the same time
     add: (state, { payload }) => {
