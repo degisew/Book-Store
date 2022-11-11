ad3483ed16207a64c82fb62afc66df3fe264628b
Degisew <jegisew21@gmail.com>
2022-11-11 20:02:08 +0300

    fix linter issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import BookStore from "./Components/BookStore";
-import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import BookStore from './Components/BookStore';
+import store from './redux/configureStore';
+import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -14,5 +14,5 @@ root.render(
         <BookStore />
       </Router>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
