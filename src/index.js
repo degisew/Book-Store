a3bcfa4d6471cfc831b71e370909a163ed62ef8f
Degisew <jegisew21@gmail.com>
2022-11-11 19:58:45 +0300

    add react-redux provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BookStore from './Components/BookStore';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import BookStore from "./Components/BookStore";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <BookStore />
-    </Router>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <Router>
+        <BookStore />
+      </Router>
+    </Provider>
+  </React.StrictMode>
 );
